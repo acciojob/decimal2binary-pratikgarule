d876828df2cb9526b39ed4bcb8949189661b1312
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-05-02 10:58:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function threeSum(arr, target) {
 // write your code here
-  
+  // JavaScript Program to convert decimal to binary
+
+const num = parseInt(prompt('Enter a decimal number: '));
+
+
+const result = num.toString(2);
+
+console.log('Binary:' + ' ' + result);
 }
 
 module.exports = threeSum;
