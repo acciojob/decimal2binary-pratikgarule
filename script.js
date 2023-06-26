5c41c3048d398b1959e6e62d4c9b1a5709880023
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-06-26 08:47:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-function decimalToBinary(n) {
-  let binary = '';
-  let quotient = n;
-
-  while (quotient !== 0) {
-    const remainder = quotient % 2;
-    binary = remainder + binary;
-    quotient = Math.floor(quotient / 2);
-  }
-
-  return binary;
+function threeSum(arr, target) {
+// write your code here
+	
+  const result = arr.toString(2);
+	return result;
 }
-    
-  
- // return parseInt(binaryStr.split("").reverse().join(""))
- 
-module.exports = decimalToBinary;
+
+module.exports = threeSum;
