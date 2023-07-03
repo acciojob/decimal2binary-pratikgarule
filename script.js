1c560d59070b9ee1f6c6f78883b6e3c8832c9ddc
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-07-03 10:58:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function threeSum(arr, target) {
-// write your code here
-  var dec=dec.toString(2)
-	return dec;
-}
+function number(num){
+    let add = "";
+        while(num > 0){
+            
+            let str = num%2;
+            add = str.toString() + add;
+
+            num =Math.floor(num/2);
+          
+        }
+        return add;
+    
+    }
 
 module.exports = threeSum;
